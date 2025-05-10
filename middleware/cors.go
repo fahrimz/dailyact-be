49bd2b1ecddf4ba384a2855c9a115a5e9f41317a
fahrimz <mzfahri620@gmail.com>
2025-05-10 16:59:50 +0700

    feat: improve CORS middleware to dynamically set allowed origins

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,9 +6,15 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set(
-			"Access-Control-Allow-Origin",
-			"http://localhost:3000, https://fahrimz.github.io") // )
+		// Set allowed origins
+		origin := c.Request.Header.Get("Origin")
+		allowedOrigins := []string{"http://localhost:3000", "https://fahrimz.github.io/dailyact-fe"}
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
